eaeb442a70e3ce139d7d4e909c55204aedd4922d
mejiamariano <mejiamariano8.08@gmail.com>
2015-04-14 03:43:29 +0000

    initial commit

---
+++ talk/examples/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/bin/AndroidManifest.xml : \
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/AndroidManifest.xml \
---
+++ talk/examples/android/bin/AppRTCDemo-debug-unaligned.apk.d
@@ -0,0 +1,6 @@
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/bin/AppRTCDemo-debug-unaligned.apk : \
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/bin/AppRTCDemo.ap_ \
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/bin/classes.dex \
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/libs/libjingle_peerconnection.jar \
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/libs/autobanh.jar \
+/home/ubuntu/webrtc-build-scripts/android/webrtc/src/talk/examples/android/libs/x86_64/libjingle_peerconnection_so.so \
